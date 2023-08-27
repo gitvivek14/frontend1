c7c59852e7a60e987d065ba5a4ee2591683c7b89
gitvivek14 <118727613+gitvivek14@users.noreply.github.com>
2023-08-27 10:34:38 +0530

    Update Endgame.jsx

--- src/pages/Endgame.jsx
+++ src/pages/Endgame.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
-
+import React , {useState , useEffect, useMemo} from 'react'
+import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector'
+import _ from 'underscore';
 const Endgame = () => {
+  const {game} = useSelector((state)=>state.auth);
+  const {signupData} = useSelector((state)=>state.auth , _.isEqual);
   return (
-    <div>Endgame</div>
+   <div className='w-full bg-richblack-900 h-screen flex items-center justify-center  flex-col'>
+    <div className='flex items-center justify-center mx-auto my-auto text-white gap-x-2 text-3xl flex-col'>
+      <div className='flex gap-x-2'>
+      <div className='text-white'>
+      <h1>
+        TeamName :
+      </h1>
+
+      </div>
+      
+     
+      <div>
+      {
+        signupData.teamName
+      }
+
+      </div>
+
+      </div>
+     
+
+      <div className='flex gap-x-2'>
+        <p>Your Total Score is : </p>
+        <div>
+        <p>
+          {
+            game?.teamPoints
+          }
+        </p>
+        </div>
+        
+
+      </div>
+
+      <div className='mt-10 flex flex-col'>
+        <div>
+        <p>
+          Thank You For Participating!!!
+         
+        </p>
+
+        </div>
+        <div>
+        <p>
+         We Hope You Enjoyed the Event.
+        </p>
+        </div>
+       
+      </div>
+    </div>
+   </div>
   )
 }
 
