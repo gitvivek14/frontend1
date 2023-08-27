ba0fe01c766580588df65465a91a4786366b263f
gitvivek14 <118727613+gitvivek14@users.noreply.github.com>
2023-08-27 10:34:58 +0530

    Update Game.jsx

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -4,35 +4,38 @@ import { useDispatch, useSelector } from 'react-redux';
 import _ from 'underscore';
 import RenderQues from '../components/RenderQues';
 import {getnewquestion} from '../services/operations/authApi'
-import { json } from 'react-router-dom';
+import {setquestionno} from '../slices/authSlice'
+
 import Loading from '../components/Loading';
 const Game = () => {
-    const dispatch = useDispatch();
+    const dispatch = useDispatch()
     const [Question, setQuestion] = useState("#244");
     const [formdata, setformdata] = useState({check:false})
     const [signupData1, setSignupData] = useState(null);
     const [questionno, setquestionno] = useState("1");
     const {signupData} = useSelector((state)=>state.auth , _.isEqual);
-    const{game} = useSelector((state)=>state.auth);
+    const{game} = useSelector((state)=>state.auth)
     const{questionnoo} = useSelector((state)=>state.auth)
     const{loading} = useSelector((state)=>state.auth)
+    
+
+
     useEffect(()=>{
-        setTimeout(()=>{
-            console.log("trying ")
-        },5000)
-        dispatch(getnewquestion(parseInt(questionno)))
-    },[questionnoo])
+        dispatch(getnewquestion(parseInt(questionno)));
+       
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[questionnoo,dispatch,questionno]);
 
     console.log("ques no changed",questionnoo);
     console.log("sign up",signupData);
 
-    if(loading==true){
+    if(loading===true){
         return(
             <Loading></Loading>
         )
     }
   return (
-    <div className='w-full'>
+    <div className="w-full">
     <div className='w-11/12 flex items-center justify-evenly mx-auto max-w-[1080px] p-8 
      relative  bg-richblack-700 text-richblack-5 mt-4 border-2 rounded-3xl border-richblack-300'>
         <div className='font-semibold'>
@@ -50,15 +53,13 @@ const Game = () => {
                 Score: {game.teamPoints}
             </p>
         </div>
-
+       
     </div>
-    {/* <div className='w-11/12 flex flex-col items-center justify-center mx-auto'>
-        <div className='mt-14 bg-slate-800 text-richblack-25 align-middle p-5 border rounded-3xl hover:bg-black'>
-        <button >Start the Game</button>
-        </div>
-        
-    </div> */}
-    <RenderQues setquestionno={setquestionno} email1={signupData.email1} max={game.teamPoints} questionno={questionno}></RenderQues>
+    {
+        questionno && <RenderQues setquestionno={setquestionno} email1={signupData.email1} max={game.teamPoints} questionno={questionno}></RenderQues>
+    }
+    
+    
 </div>
   )
 }
